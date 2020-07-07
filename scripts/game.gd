ca6ce93e82cfc2ec29c61eaf109b559ff510a82f
Leandro Conca De Luccia <lluccia@gmail.com>
2020-07-06 23:22:02 -0300

    implements score

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,11 +1,26 @@
 extends Node2D
 
+signal game_over
+
+var _ball_start_position = null
+
+var _p1_score = 0
+var _p2_score = 0
+var _max_score = 10
+
 func _ready():
 	$Ball.set_speed(300)
 	$Ball.set_direction(Vector2(1, -1))
+	_ball_start_position = $Ball.get_position()
 
 	$P1Paddle.set_speed(300)
 	$P2Paddle.set_speed(300)
+	
+	_update_display()
+
+func _update_display():
+	$P1Score.set_text(str(_p1_score))
+	$P2Score.set_text(str(_p2_score))
 
 func _process(delta):
 	if Input.is_action_pressed("p1_up"):
@@ -16,3 +31,42 @@ func _process(delta):
 		$P2Paddle.move_up(delta)
 	if Input.is_action_pressed("p2_down"):
 		$P2Paddle.move_down(delta)
+
+func _on_P1KillBox_kill_ball():
+	$Ball.set_position(_ball_start_position)
+	_p2_score += 1
+	_update_display()
+	if _p2_score == _max_score:
+		_game_over()
+
+func _on_P2KillBox_kill_ball():
+	$Ball.set_position(_ball_start_position)
+	_p1_score += 1
+	_update_display()
+	if _p1_score == _max_score:
+		_game_over()
+
+func _game_over():
+	emit_signal('game_over')
+	$Ball.set_speed(0)
+
+func get_ball():
+	return $Ball
+
+func get_p1_score():
+	return _p1_score
+
+func set_p1_score(p1_score):
+	_p1_score = p1_score
+
+func get_p2_score():
+	return _p2_score
+
+func set_p2_score(p2_score):
+	_p2_score = p2_score
+
+func get_max_score():
+	return _max_score
+
+func set_max_score(max_score):
+	_max_score = max_score
---
+++ test/integration/test_game.gd
@@ -0,0 +1,95 @@
+extends 'res://addons/gut/test.gd'
+
+var GameScene = load('res://scenes/Game.tscn')
+
+var _game
+
+func before_each():
+	_game = GameScene.instance()
+	add_child(_game)
+
+func after_each():
+	remove_child(_game)
+
+func _p1_scores():
+	_game.get_node("P2KillBox").emit_signal('kill_ball')
+	
+func _p2_scores():
+	_game.get_node("P1KillBox").emit_signal('kill_ball')
+	
+func test_get_set_p1_score():
+	assert_accessors(_game, 'p1_score', 0, 10)
+
+func test_get_set_p2_score():
+	assert_accessors(_game, 'p2_score', 0, 10)
+
+func test_get_set_max_score():
+	assert_accessors(_game, 'max_score', 10, 0)
+
+func test_when_p1_killbox_emits_kill_ball_then_ball_is_recentered():
+	var ball = _game.get_ball() 
+	var orig_pos = ball.get_position()
+	
+	ball.set_position(Vector2(1,1))
+	
+	_p2_scores()
+	assert_eq(ball.get_position(), orig_pos)
+
+func test_when_p2_killbox_emits_kill_ball_then_ball_is_recentered():
+	var ball = _game.get_ball() 
+	var orig_pos = ball.get_position()
+	
+	ball.set_position(Vector2(1,1))
+	
+	_p1_scores()
+	assert_eq(ball.get_position(), orig_pos)
+
+func test_when_p1_killbox_kills_ball_p2_score_increases():
+	_p2_scores()
+	
+	assert_eq(_game.get_p2_score(), 1)
+
+func test_when_p2_killbox_kills_ball_p1_score_increases():
+	_p1_scores()
+	
+	assert_eq(_game.get_p1_score(), 1)
+
+func test_score_labels_show_score_on_start():
+	assert_eq(_game.get_node("P1Score").text, "0")
+	assert_eq(_game.get_node("P2Score").text, "0")
+
+func test_when_p1_scores_then_score_is_updated():
+	_p1_scores()
+	assert_eq(_game.get_node("P1Score").text, "1")
+
+func test_when_p2_scores_then_score_is_updated():
+	_p2_scores()
+	assert_eq(_game.get_node("P2Score").text, "1")
+
+func test_when_p1_reaches_max_score_game_over_emmited():
+	watch_signals(_game)
+	_game.set_max_score(2)
+
+	_p1_scores()
+	_p1_scores()
+
+	assert_signal_emitted(_game, 'game_over')
+
+func test_when_p2_reaches_max_score_game_over_emmited():
+	watch_signals(_game)
+	_game.set_max_score(3)
+
+	_p2_scores()
+	_p2_scores()
+	_p2_scores()
+
+	assert_signal_emitted(_game, 'game_over')
+
+func test_when_game_ends_ball_stops_moving():
+	_game.set_max_score(2)
+
+	_p1_scores()
+	_p1_scores()
+	
+	assert_eq(_game.get_ball().get_speed(), 0)
+	
